5670c3ba642d43a2b8df0beb4e3942983b42899f
Sandhya95Patil <sandhya364@gmail.com>
2024-03-08 18:18:55 +0530

    Linear Search Commited

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace linearSearch
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the  size of array:- ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the elements in array :- ");
+            int[] array = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                array[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine("Enter the element to search :- ");
+            int item=Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] == item)
+                {
+                    Console.WriteLine("item is present at " + i + " index position.");
+                }
+            }
+        }
+    }
+}
