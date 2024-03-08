42ca7161fa687756d95f0519a12bd2e12b79b31f
Sandhya95Patil <sandhya364@gmail.com>
2024-03-08 18:21:31 +0530

    Insertion Sort Commited

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace insertionSort
+{
+  public class Program
+    {
+        static void Main(string[] args)
+        {
+            int temp, j;
+            Console.WriteLine("Enter the  size of array:- ");
+            int n=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the elements in array :- ");
+            int[] array= new int[n];
+            for(int i=0; i<n; i++)
+            {
+                array[i]=Convert.ToInt32(Console.ReadLine());
+            }
+            for( int i = 1; i < array.Length; i++)
+            {
+                temp = array[i];
+                j = i;
+                while(j > 0  && array[j-1]>temp)
+                {
+                    array[j] = array[j-1];
+                    j--;
+                }
+            }
+            for (int i = 1;i< array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+
+
+
+            }
+        }
+    }
+}
