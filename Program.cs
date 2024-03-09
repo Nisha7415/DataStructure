5aa8457f8a2f6287fec7a89f2cf96d1ff50f8c30
Sandhya95Patil <sandhya364@gmail.com>
2024-03-09 19:37:34 +0530

    Hashtable Commited

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+
+namespace hashTable
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Hashtable Htable=new Hashtable();
+            Htable.Add(1, 23);
+            Htable.Add(2, "a");
+            Htable.Add(3, 45);
+            Htable.Add(4, "c");
+            //foreach(object i in Htable.Keys)
+            //{
+            //    Console.WriteLine(i);
+            //}
+            //foreach(object j in Htable.Values)
+            //{
+            //    Console.WriteLine(j);
+               
+            //}
+            foreach (DictionaryEntry dic in Htable)
+            {
+
+                Console.WriteLine("keys={0}  values={1}", dic.Key, dic.Value);
+               
+
+            }
+            Console.WriteLine(Htable.Contains(5));
+            Console.WriteLine(Htable.ContainsValue(45));
+           
+
+        }
+    }
+}
