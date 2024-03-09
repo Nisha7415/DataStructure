c36c85c631105aaea41792556b2024782e044a1e
Sandhya95Patil <sandhya364@gmail.com>
2024-03-09 19:27:51 +0530

    Hashmap Commited

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+
+namespace DS_Hashmap
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Dictionary<string, int> hashMap = new Dictionary<string, int>();
+            hashMap.Add("nisha", 1);
+            hashMap.Add("mamata",2);
+            hashMap.Add("pooja",3);
+            Console.WriteLine("Elements in hashmap are :- ");
+            foreach(var i in  hashMap)
+            {
+                Console.WriteLine(i);
+            }
+            hashMap.Remove("nisha");
+            Console.WriteLine("After removing the element:- ");
+            foreach (var i in hashMap)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("Count of hashmap:- "+hashMap.Count);
+        }
+    }
+}
